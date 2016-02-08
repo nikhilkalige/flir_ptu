1b61c79d5b7bb94971c3cffbfce88e6b3c38571e
nikhilkalige <nikhilkalige@gmail.com>
2016-02-08 11:22:55 -0700

    Add a few test commands

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
-from flir.stream import Stream
-from flir.flir import FLIR
-import time
+from ptu.ptu import PTU
+import logging
 
 
-#h = Stream("129.219.136.149", 4000)
-#h.connect()
-#time.sleep(5)
-#h.write("PP-500".encode("ascii"))
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(levelname)s:%(name)s:- %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
-x = FLIR("129.219.136.149", 4000)
+logger.setLevel(logging.DEBUG)
+
+x = PTU("129.219.136.149", 4000)
 x.connect()
 
-value = True
-#value = False
+value = 3
 
-if value:
-    x.pan(0)
+if value == 1:
+    x.pan(25)
     print(x.pan())
-else:
+elif value == 2:
     x.pan_angle(45)
     print(x.pan())
+elif value == 3:
+    x.pan_offset(-10)
+    print("XX", x.pan())
+
 x.stream.close()
